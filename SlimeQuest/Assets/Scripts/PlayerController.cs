697783d99dd93a1129faf01459a04d6bab89845b
Alexis Lee <aclee2001@gmail.com>
2022-11-05 17:45:25 -0700

    Fixed for continuous movement

--- SlimeQuest/Assets/Scripts/PlayerController.cs
+++ SlimeQuest/Assets/Scripts/PlayerController.cs
@@ -23,29 +23,29 @@ public class PlayerController : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed*Time.deltaTime);
 
-        if(Input.anyKeyDown){
-            if(Vector3.Distance(transform.position, movePoint.position) <= 0.05f){
-                if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f){
-                    if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, whatStopsMovement)){
-                        movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);                                           
+        
+        if(Vector3.Distance(transform.position, movePoint.position) <= 0.05f){
+            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f){
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, whatStopsMovement)){
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);                                           
 
-                    }
-                
                 }
             
-                else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f){
-                    if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, whatStopsMovement)){
-                        movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);                            
-                                   
-                    }
-                
+            }
+        
+            else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f){
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, whatStopsMovement)){
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);                            
+                                
                 }
+            
             }
-            Vector3 moveDirection = transform.position - movePoint.position;
-            if (moveDirection != Vector3.zero){
-            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x)*Mathf.Rad2Deg + 90;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            }
+        }
+        Vector3 moveDirection = transform.position - movePoint.position;
+        if (moveDirection != Vector3.zero){
+        float angle = Mathf.Atan2(moveDirection.y, moveDirection.x)*Mathf.Rad2Deg + 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            
                
     
 
