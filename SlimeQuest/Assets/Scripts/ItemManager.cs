add09e742a0da71cbaf8f4f8424bf94770a001e4
Anika Richards <109831070+anika-26@users.noreply.github.com>
2022-11-06 17:20:14 -0800

    fixing placement of objects

--- SlimeQuest/Assets/Scripts/ItemManager.cs
+++ SlimeQuest/Assets/Scripts/ItemManager.cs
@@ -23,16 +23,16 @@ public class ItemManager : MonoBehaviour
     void Start()
     {   
         
-        Instantiate(IngredientOne, new Vector3(5, 5, 0), Quaternion.identity);
-        Instantiate(IngredientTwo, new Vector3(10, 10,0), Quaternion.identity);
-        Instantiate(IngredientThree, new Vector3(-7,-8, 0), Quaternion.identity);
+        Instantiate(IngredientOne, new Vector3(50, -5, 0), Quaternion.identity);
+        Instantiate(IngredientTwo, new Vector3(25, 10,0), Quaternion.identity);
+        Instantiate(IngredientThree, new Vector3(8,-20, 0), Quaternion.identity);
 
         //instantiate dragon items 
-        Instantiate(dragonItemOne, new Vector3(-14,-8, 0), Quaternion.identity);
-        Instantiate(dragonItemTwo, new Vector3(14,-8, 0), Quaternion.identity);
-        Instantiate(dragonItemThree, new Vector3(2,-14, 0), Quaternion.identity);
-        Instantiate(dragonItemFour, new Vector3(3,-10, 0), Quaternion.identity);
-        Instantiate(dragonItemFive, new Vector3(-10,10, 0), Quaternion.identity);
+        Instantiate(dragonItemOne, new Vector3(30,-10, 0), Quaternion.identity);
+        Instantiate(dragonItemTwo, new Vector3(45,-20, 0), Quaternion.identity);
+        Instantiate(dragonItemThree, new Vector3(52,14, 0), Quaternion.identity);
+        Instantiate(dragonItemFour, new Vector3(-5,-12, 0), Quaternion.identity);
+        Instantiate(dragonItemFive, new Vector3(10,12, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
