ce536659c5760c15560ff01811b7947dd9f59ff8
Alexis Lee <aclee2001@gmail.com>
2022-11-07 12:37:37 -0800

    Removed contraint of non diagonal movement

--- SlimeQuest/Assets/Scripts/PlayerController.cs
+++ SlimeQuest/Assets/Scripts/PlayerController.cs
@@ -40,7 +40,7 @@ public class PlayerController : MonoBehaviour
             
             }
         
-            else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f){
+            if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f){
                 if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, whatStopsMovement)){
                     movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);                            
                                 
