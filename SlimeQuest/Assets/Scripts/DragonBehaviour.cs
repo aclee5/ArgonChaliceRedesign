f410b311e0206c96e798d3b9c3ebc27477cfeabb
Alexis Lee <aclee2001@gmail.com>
2022-11-11 13:51:51 -0800

    Options Placed

--- SlimeQuest/Assets/Scripts/DragonBehaviour.cs
+++ SlimeQuest/Assets/Scripts/DragonBehaviour.cs
@@ -7,22 +7,31 @@ public class DragonBehaviour : Character
     public int giftNum; 
     private int giftTotal; 
     public DialogueTrigger introTrigger; 
+    public GameObject dialogueBox; 
+    public GameObject inventoryOptionButton;
+    public GameObject noItemOption; 
 
     // Start is called before the first frame update
     void Start()
     {
-        giftNum = 0; 
+        giftNum = 0;
+        dialogueBox.SetActive(false); 
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(dialogueBox.GetComponent<DialogueManager>().conversationFinished){
+            inventoryOptionButton.SetActive(true);
+            noItemOption.SetActive(true); 
+        }
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision){
+    private void OnTriggerEnter2D(Collider2D collision){
         if(collision.gameObject.CompareTag("Player") == true){
+            dialogueBox.SetActive(true); 
             introTrigger.StartDialogue();
 
         }
