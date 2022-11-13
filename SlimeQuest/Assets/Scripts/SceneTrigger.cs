c363f6a1730107b7f1cd907c7efc23c16c1fdb88
errr1n <eteply@telus.net>
2022-11-12 16:46:48 -0800

    level change

---
+++ SlimeQuest/Assets/Scripts/SceneTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTrigger : MonoBehaviour
+{
+   // public int sceneState; 
+    public int nextSceneBuildIndex;
+    
+    void OnTriggerEnter2D(Collider2D collision){
+        if(collision.CompareTag("Player")){
+            //FindObjectOfType<SaveSystem>().SavePlayerDataTo(nextSceneBuildIndex); 
+           // FindObjectOfType<GameManager>().loadScene(sceneState); 
+
+           SceneManager.LoadScene(nextSceneBuildIndex, LoadSceneMode.Single);
+
+        }
+
+    }
+}
