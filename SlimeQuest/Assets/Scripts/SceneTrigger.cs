63d96bf814319cc6a97118cdcd2990be20a7cdd7
Alexis Lee <aclee2001@gmail.com>
2022-11-12 17:49:38 -0800

    Unfucking the scene  trigger script

--- SlimeQuest/Assets/Scripts/SceneTrigger.cs
+++ SlimeQuest/Assets/Scripts/SceneTrigger.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.SceneManagement;
 
 public class SceneTrigger : MonoBehaviour
 {
    // public int sceneState; 
     public int nextSceneBuildIndex;
+    public int sceneState; 
+    public bool insideLevel; 
     
     void OnTriggerEnter2D(Collider2D collision){
         if(collision.CompareTag("Player")){
-          //  FindObjectOfType<SaveSystem>().SavePlayerDataTo(nextSceneBuildIndex); 
-           // FindObjectOfType<GameManager>().loadScene(sceneState); 
+            if(insideLevel){
+                SceneManager.LoadScene(nextSceneBuildIndex, LoadSceneMode.Single);
+            }
+            else{
+                FindObjectOfType<SaveSystem>().SavePlayerDataTo(nextSceneBuildIndex); 
+                FindObjectOfType<GameManager>().loadScene(sceneState); 
+
+            }
+          
             
-            SceneManager.LoadScene(nextSceneBuildIndex, LoadSceneMode.Single);
+            
 
         }
 
