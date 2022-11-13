638b4352599d885d948bd95729ddd729bb2ab1c7
Alexis Lee <aclee2001@gmail.com>
2022-11-13 12:34:56 -0800

    Sign Is Active on Player Enter

--- SlimeQuest/Assets/Scripts/Sign.cs
+++ SlimeQuest/Assets/Scripts/Sign.cs
@@ -7,47 +7,64 @@ using TMPro;
 public class Sign : MonoBehaviour
 {
 
-    public GameObject dialogBox;
-    public TMP_Text dialogText;
-    public string dialog;
-    public bool playerInRange;
-
+    public DialogueTrigger trigger;
+    public GameObject dialogueBox; 
     // Start is called before the first frame update
-    void Start()
-    {
+    void OnTriggerEnter2D(Collider2D collision){
+        if(collision.CompareTag("Player")){
+            dialogueBox.SetActive(true); 
+            trigger.StartDialogue(); 
+            
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision){
+        if(collision.CompareTag("Player")){
+            dialogueBox.SetActive(false); 
+        }
+    }
+
+    // public GameObject dialogBox;
+    // public TMP_Text dialogText;
+    // public string dialog;
+    // public bool playerInRange;
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
         
-    }
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKey(KeyCode.Space) && playerInRange)
-        {
-            if(dialogBox.activeInHierarchy)
-            {
-                dialogBox.SetActive(false);
-            }
-            else {
-                dialogBox.SetActive(true);
-                dialogText.text = dialog;
-            }
-        }
-    }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if(Input.GetKey(KeyCode.Space) && playerInRange)
+    //     {
+    //         if(dialogBox.activeInHierarchy)
+    //         {
+    //             dialogBox.SetActive(false);
+    //         }
+    //         else {
+    //             dialogBox.SetActive(true);
+    //             dialogText.text = dialog;
+    //         }
+    //     }
+    // }
 
-    public void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.CompareTag("Player"))
-        {
-            playerInRange = true;
-            Debug.Log("Player in range");
-        }
-    }
+    // public void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     if(other.CompareTag("Player"))
+    //     {
+    //         playerInRange = true;
+    //         Debug.Log("Player in range");
+    //     }
+    // }
     
-    public void OnTriggerExit2D(Collider2D other){
-        if(other.CompareTag("Player")){
-            playerInRange = false;
-            Debug.Log("PLayer left  range");
-        }
-    }
+    // public void OnTriggerExit2D(Collider2D other){
+    //     if(other.CompareTag("Player")){
+    //         playerInRange = false;
+    //         Debug.Log("PLayer left  range");
+    //     }
+    // }
 
 }
