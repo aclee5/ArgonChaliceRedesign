d81af4e50f8798702b6b3c42409e6da6b27a9cd1
Anika Richards <109831070+anika-26@users.noreply.github.com>
2022-11-18 14:37:55 -0800

    dialogue pop up triggerin

--- SlimeQuest/Assets/Scripts/Riddles.cs
+++ SlimeQuest/Assets/Scripts/Riddles.cs
@@ -8,16 +8,7 @@ public class Riddles : MonoBehaviour
     public DialogueTrigger trigger;
     public GameObject dialogueBox; 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 
     void OnTriggerEnter2D(Collider2D collision){
         if(collision.CompareTag("Player")){
