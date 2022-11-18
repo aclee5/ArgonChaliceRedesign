8fd894f5206ea9df29eba8a7a27aa22628274b30
Alexis Lee <aclee2001@gmail.com>
2022-11-18 14:57:11 -0800

    Obstacles reset to checkpoint, and can move dynamically

--- SlimeQuest/Assets/Scripts/ObstacleGenerator.cs
+++ SlimeQuest/Assets/Scripts/ObstacleGenerator.cs
@@ -5,35 +5,38 @@ using UnityEngine;
 public class ObstacleGenerator : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    public float courseWidth; 
-    public float courseHeight;
+    public GameObject courseBounds; 
+    private float courseWidth; 
+    private float courseHeight;
  
 
     // Start is called before the first frame update
     void Start()
     {
+        courseWidth = courseBounds.transform.localScale.x;
+        courseHeight = courseBounds.transform.localScale.y; 
        
   
 
         float gap = 4; 
-        for(int x = 0; x < width; x ++){
+        for(int x = 0; x < courseWidth; x ++){
     
             if (x%gap == 0){
-            float maximumBarrierHeightRatio = 0.8f;
-            if(x < 0.2*courseWidth){
-                maximumBarrierHeightRatio = 0.5f; 
+            float maximumBarrierHeightRatio = 0.6f;
+            if(x < 0.1*courseWidth){
+                maximumBarrierHeightRatio = 0.4f; 
             }
             float yScale = Random.Range(0,maximumBarrierHeightRatio); 
             float barrierHeight = yScale*courseHeight; 
             float middleOfBarrier = barrierHeight/2; 
 
             float randomPlacement = (float) Random.Range(0, 2);
-            if(Random.Range(0,2) < 1){
+            if(Random.Range(0,2) < 1 && x > (0.1*courseWidth)){
                 middleOfBarrier = courseHeight - middleOfBarrier; 
             }
             
 
-            GameObject barrier = Instantiate(obstaclePrefab, new Vector3(x, middleOfBarrier, 0), Quaternion.identity);
+            GameObject barrier = Instantiate(obstaclePrefab, new Vector3(x + 0.5f, middleOfBarrier, 0), Quaternion.identity);
             barrier.transform.localScale = new Vector3(1f, barrierHeight, 1f); 
         }
 
