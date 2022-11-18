2f503dff33ea3b547f79fda2936ced6ac9a904f6
Alexis Lee <aclee2001@gmail.com>
2022-11-18 13:42:05 -0800

    AddDirtTileShit

--- SlimeQuest/Assets/Scripts/ObstacleGenerator.cs
+++ SlimeQuest/Assets/Scripts/ObstacleGenerator.cs
@@ -7,44 +7,42 @@ public class ObstacleGenerator : MonoBehaviour
     public GameObject obstaclePrefab;
     public float courseWidth; 
     public float courseHeight;
-    private float width, height; 
  
 
     // Start is called before the first frame update
     void Start()
     {
-        width = (float)(courseWidth + 0.2*courseWidth);
-        height = (float)(courseHeight + 2);  
+       
   
 
-        float gap = 3; 
+        float gap = 4; 
         for(int x = 0; x < width; x ++){
-            if(x != 0 || x != width){
-                if (x%gap == 0){
-                float maximumBarrierHeightRatio = 0.8f;
-                if(x < 0.2*courseWidth){
-                    maximumBarrierHeightRatio = 0.5f; 
-                }
-                float yScale = Random.Range(0,maximumBarrierHeightRatio); 
-                float barrierHeight = yScale*courseHeight; 
-                float middleOfBarrier = barrierHeight/2; 
-
-                float randomPlacement = (float) Random.Range(0, 2);
-                if(Random.Range(0,2) < 1){
-                    middleOfBarrier = courseHeight - middleOfBarrier; 
-                }
-                
-
-                GameObject barrier = Instantiate(obstaclePrefab, new Vector3(x, middleOfBarrier, 0), Quaternion.identity);
-                barrier.transform.localScale = new Vector3(1f, barrierHeight, 1f); 
+    
+            if (x%gap == 0){
+            float maximumBarrierHeightRatio = 0.8f;
+            if(x < 0.2*courseWidth){
+                maximumBarrierHeightRatio = 0.5f; 
             }
+            float yScale = Random.Range(0,maximumBarrierHeightRatio); 
+            float barrierHeight = yScale*courseHeight; 
+            float middleOfBarrier = barrierHeight/2; 
 
-            if(x == (int)courseWidth/4 || x == (int)courseWidth*0.75){
-                gap -= 1; 
-           
+            float randomPlacement = (float) Random.Range(0, 2);
+            if(Random.Range(0,2) < 1){
+                middleOfBarrier = courseHeight - middleOfBarrier; 
             }
+            
 
-            }
+            GameObject barrier = Instantiate(obstaclePrefab, new Vector3(x, middleOfBarrier, 0), Quaternion.identity);
+            barrier.transform.localScale = new Vector3(1f, barrierHeight, 1f); 
+        }
+
+        if(x == (int)courseWidth/4 || x == (int)courseWidth*0.75){
+            gap -= 1; 
+        
+        }
+
+            
             
             
         }
