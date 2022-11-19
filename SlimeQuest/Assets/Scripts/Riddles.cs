ba46a220fda025e83900fd70a817cf389916e316
errr1n <eteply@telus.net>
2022-11-18 18:02:25 -0800

    Revert "Merge branch 'main' of https://github.com/aclee5/ArgonChaliceRedesign"
    This reverts commit 1f2e2e147461667353961fe0828db2a6e01b62ee, reversing
    changes made to 084d8b453529b4770d6d1b07385e303feb5593c0.

--- SlimeQuest/Assets/Scripts/Riddles.cs
+++ SlimeQuest/Assets/Scripts/Riddles.cs
@@ -1,61 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class Riddles : MonoBehaviour
 
 {
     public DialogueTrigger trigger;
-    public DialogueTrigger riddle2Trigger;
-
     public GameObject dialogueBox; 
-    public GameObject answerOneButton;
-    public GameObject answerTwoButton;
-    public GameObject answerThreeButton;
-    public bool correctRiddle; // to check if they correctly did riddle 
-    public int convoProgress;
-
-    //texts for the buttton blobbs
-    public TMP_Text answerOne;
-    public TMP_Text answerTwo;
-    public TMP_Text answerThree;
+    // Start is called before the first frame update
    
-    void Start(){
-        //Debug.Log("hi");
-       correctRiddle = false;
-       convoProgress = 0;
-       answerOneButton.SetActive(false);
-       answerThreeButton.SetActive(false);
-       answerTwoButton.SetActive(false);
-    }
-
-    void Update(){
-        if(correctRiddle){
-            Debug.Log("answer correct");
-            convoProgress =1;
-            
-        } 
-
-        if(dialogueBox.GetComponent<DialogueManager>().conversationFinished ){
-           
-            answerOneButton.SetActive(true);
-            answerTwoButton.SetActive(true);
-            answerThreeButton.SetActive(true);
-
-            
-           
-        } else {
-            answerOneButton.SetActive(false);
-            answerTwoButton.SetActive(false);
-            answerThreeButton.SetActive(false);
-        }
-    }
 
     void OnTriggerEnter2D(Collider2D collision){
         if(collision.CompareTag("Player")){
-            dialogueBox.SetActive(true);
-            //answerOneButton.SetActive(true);  
+            dialogueBox.SetActive(true); 
             trigger.StartDialogue(); 
             
         }
@@ -66,25 +23,4 @@ public class Riddles : MonoBehaviour
             dialogueBox.SetActive(false); 
         }
     }
-
-    public void answerOne(){
-        //method to deal with option 1
-        Debug.Log("pressed option 1");
-        //correct asnwer 
-        correctRiddle = true; 
-        StartConvo(convoProgress);
-        //answerOneButton.SetActive(false);
-    
-    }
-
-
-    public void StartConvo(int num){
-        switch (num)
-        {
-            case 1:
-                riddle2Trigger.StartDialogue();
-                break; 
-            
-        }
-}
 }
