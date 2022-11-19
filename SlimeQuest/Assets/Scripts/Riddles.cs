0b6d04a877d51419513fc2b8012be0e44a605732
Anika Richards <109831070+anika-26@users.noreply.github.com>
2022-11-18 17:29:53 -0800

    just dont wanna lose progress riddles

--- SlimeQuest/Assets/Scripts/Riddles.cs
+++ SlimeQuest/Assets/Scripts/Riddles.cs
@@ -6,13 +6,41 @@ public class Riddles : MonoBehaviour
 
 {
     public DialogueTrigger trigger;
+    public DialogueTrigger riddle2Trigger;
+
     public GameObject dialogueBox; 
-    // Start is called before the first frame update
+    public GameObject answerOneButton;
+    public bool correctRiddle; // to check if they correctly did riddle 
+    public int convoProgress;
    
+    void Start(){
+        //Debug.Log("hi");
+       correctRiddle = false;
+       convoProgress = 0;
+       answerOneButton.SetActive(false);
+    }
+
+    void Update(){
+        if(correctRiddle){
+            Debug.Log("answer correct");
+            convoProgress =1;
+            
+        }
+
+        if(dialogueBox.GetComponent<DialogueManager>().conversationFinished ){
+           
+                answerOneButton.SetActive(true);
+            
+           
+        } else {
+            answerOneButton.SetActive(false);
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D collision){
         if(collision.CompareTag("Player")){
-            dialogueBox.SetActive(true); 
+            dialogueBox.SetActive(true);
+            //answerOneButton.SetActive(true);  
             trigger.StartDialogue(); 
             
         }
@@ -23,4 +51,25 @@ public class Riddles : MonoBehaviour
             dialogueBox.SetActive(false); 
         }
     }
+
+    public void answerOne(){
+        //method to deal with option 1
+        Debug.Log("pressed option 1");
+        //correct asnwer 
+        correctRiddle = true; 
+        StartConvo(convoProgress);
+        //answerOneButton.SetActive(false);
+    
+    }
+
+
+    public void StartConvo(int num){
+        switch (num)
+        {
+            case 1:
+                riddle2Trigger.StartDialogue();
+                break; 
+            
+        }
+}
 }
