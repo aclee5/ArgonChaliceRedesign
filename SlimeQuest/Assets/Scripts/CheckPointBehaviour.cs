888346a3f9bd0c6619885f9cb6b06ac036946ca2
Alexis Lee <aclee2001@gmail.com>
2022-11-20 00:38:07 -0800

    AddCounterBackIntoEndRoom

--- SlimeQuest/Assets/Scripts/CheckPointBehaviour.cs
+++ SlimeQuest/Assets/Scripts/CheckPointBehaviour.cs
@@ -7,7 +7,7 @@ public class CheckPointBehaviour : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision){
         if(collision.CompareTag("Player")){
-            if (SceneManager.GetActiveScene().buildIndex > FindObjectOfType<Player>().respawnPoints.Capacity-1){
+            if ((FindObjectOfType<Player>().respawnPoints.Capacity == 0) || (SceneManager.GetActiveScene().buildIndex > FindObjectOfType<Player>().respawnPoints.Capacity-1)){
                 FindObjectOfType<Player>().respawnPoints.Add(transform.position); 
             }
             else{
